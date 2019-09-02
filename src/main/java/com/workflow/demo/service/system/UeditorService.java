94e4bb79536e8c61f935913f08cab2fdcee6d870
chenyihong <chenyihongyi@163.com>
2019-09-02 14:52:36 +0800

    提交代码

--- src/main/java/com/workflow/demo/service/system/UeditorService.java
+++ src/main/java/com/workflow/demo/service/system/UeditorService.java
@@ -1,16 +1,40 @@
 package com.workflow.demo.service.system;
 
-import com.workflow.demo.entity.system.Ueditor;
-import com.baomidou.mybatisplus.service.IService;
+import com.workflow.demo.entity.PageData;
 
 /**
  * <p>
  *  服务类
  * </p>
- *
+ * 富文本编辑器内容缓存接口
  * @author chenyihongyi
  * @since 2019-09-01
  */
-public interface UeditorService extends IService<Ueditor> {
+public interface UeditorService {
+
+    /**通过id获取数据
+     * @param pd
+     * @throws Exception
+     */
+    public PageData findById(PageData pd)throws Exception;
+
+    /**修改
+     * @param pd
+     * @throws Exception
+     */
+    public void edit(PageData pd)throws Exception;
+
+    /**删除
+     * @param pd
+     * @throws Exception
+     */
+    public void delete(PageData pd)throws Exception;
+
+    /**批量删除
+     * @param ArrayDATA_IDS
+     * @throws Exception
+     */
+    public void deleteAll(String[] ArrayDATA_IDS)throws Exception;
+
 	
 }
